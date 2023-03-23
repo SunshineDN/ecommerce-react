cd020bf020a3de222f341cac0eab4d6e93003f3b
Douglas Augusto <douglasaugusto1103@gmail.com>
2023-03-23 15:44:24 -0300

    Adicionado header às apis

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,6 @@ export const productAPI = axios.create({
     baseURL: "https://fakestoreapi.com",
     headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
     },
 });
 
@@ -12,6 +11,5 @@ export const currencyAPI = axios.create({
     baseURL: "https://economia.awesomeapi.com.br",
     headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
     },
 });
